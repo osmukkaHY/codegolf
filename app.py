ee7d1ea0333a555708abbaf00f1634ae15e394fe
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-09-02 19:01:30 +0300

    Added endpoint for signup page

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+)
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
