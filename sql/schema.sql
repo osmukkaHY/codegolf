c74e460f5d213fa4434a8cd90f029503e4c18726
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-09-04 11:27:57 +0300

    Added description field to Posts

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,12 +1,13 @@
 CREATE TABLE Users (
     id            INTEGER PRIMARY KEY,
-    username      TEXT UNIQUE,
-    password_hash TEXT
+    username      TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
 );
 
 CREATE TABLE Posts (
-    id INTEGER PRIMARY KEY,
-    poster_id REFERENCES Users(id),
-    title TEXT
+    id          INTEGER PRIMARY KEY,
+    poster_id   REFERENCES Users(id) NOT NULL,
+    title       TEXT NOT NULL,
+    description TEXT NOT NULL
 );
 
