4668346657b3a74236c6157bb35572a2f2b5c050
osmukkaHY <oskari.lappalainen.hy@gmail.com>
2026-09-05 23:59:55 +0300

    Added script for populating the database.

---
+++ sql/populate.sql
@@ -0,0 +1,10 @@
+INSERT INTO Users (username, password_hash) VALUES ('user1', 'scrypt:32768:8:1$IUI6IDekdzOdezAT$2186fb11cdb0a7f945b90f2994541350deea6d7b78b72ecda11d5895c74b24368b6e3f3303f0f739fb326253cf803e3b9215e3c6acdc12ba815e06cf37110ebc');
+INSERT INTO Users (username, password_hash) VALUES ('user2', 'scrypt:32768:8:1$IUI6IDekdzOdezAT$2186fb11cdb0a7f945b90f2994541350deea6d7b78b72ecda11d5895c74b24368b6e3f3303f0f739fb326253cf803e3b9215e3c6acdc12ba815e06cf37110ebc');
+INSERT INTO Users (username, password_hash) VALUES ('user3', 'scrypt:32768:8:1$IUI6IDekdzOdezAT$2186fb11cdb0a7f945b90f2994541350deea6d7b78b72ecda11d5895c74b24368b6e3f3303f0f739fb326253cf803e3b9215e3c6acdc12ba815e06cf37110ebc');
+INSERT INTO Users (username, password_hash) VALUES ('user4', 'scrypt:32768:8:1$IUI6IDekdzOdezAT$2186fb11cdb0a7f945b90f2994541350deea6d7b78b72ecda11d5895c74b24368b6e3f3303f0f739fb326253cf803e3b9215e3c6acdc12ba815e06cf37110ebc');
+
+INSERT INTO Posts (poster_id, title, description) VALUES (1, 'post1', 'this is a post');
+INSERT INTO Posts (poster_id, title, description) VALUES (2, 'post2', 'this is a post');
+INSERT INTO Posts (poster_id, title, description) VALUES (3, 'post3', 'this is a post');
+INSERT INTO Posts (poster_id, title, description) VALUES (4, 'post4', 'this is a post');
+
